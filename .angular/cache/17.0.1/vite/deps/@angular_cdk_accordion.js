9980aa2844d3e33367fe3765344048163c965006
Adalbero F. Guimaraes <adalbero@gmail.com>
2023-11-19 20:31:19 +0100

    prototype 1

---
+++ .angular/cache/17.0.1/vite/deps/@angular_cdk_accordion.js
@@ -0,0 +1,15 @@
+import {
+  CDK_ACCORDION,
+  CdkAccordion,
+  CdkAccordionItem,
+  CdkAccordionModule
+} from "./chunk-5UR6OWOB.js";
+import "./chunk-3ZBPDMMR.js";
+import "./chunk-RS7OVMRY.js";
+export {
+  CDK_ACCORDION,
+  CdkAccordion,
+  CdkAccordionItem,
+  CdkAccordionModule
+};
+//# sourceMappingURL=@angular_cdk_accordion.js.map
---
+++ .angular/cache/17.0.1/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-7OJSXP2O.js";
+import "./chunk-5EUCZEFY.js";
+import "./chunk-TUXSKZU5.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-RS7OVMRY.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
