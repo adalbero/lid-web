c486ed533e4f8075c84f7627c4da4a425ac70bff
Adalbero F. Guimaraes <adalbero@gmail.com>
2023-11-19 12:30:15 +0100

    init angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'lid-web';
